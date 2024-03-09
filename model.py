f55fa899a73f248f4a3660f93d91f9225553e796
abdallah197 <abdullahbashir077@gmail.com>
2024-03-09 11:07:20 +0000

    added mtheta calualtions for ROPE

--- model.py
+++ model.py
@@ -1,9 +1,8 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import math
 from dataclasses import dataclass
 from typing import Optional
+
+import torch
+import torch.nn as nn
 
 
 @dataclass
@@ -22,6 +21,42 @@ class ModelArgs:
     max_seq_length: int = 2048
 
     device: str = None
+
+
+def precompute_theta_pos_frequencies(head_dim: int, seq_length: int, device: str, theta: float = 10000):
+    """
+    compute complex MTheta matrix
+    Args:
+        head_dim: head dimension of the attention head
+        seq_length: sequence length
+        device: device
+        theta: theta number in the paper, defaults to 10,000
+
+    Returns: matrix of complex mtheta R * exp(i m*theta), R=1
+
+    """
+    # As written in the paragraph 3.2.2 of the paper
+    # >> In order to generalize our results in 2D to any xi ∈ Rd where **d is even**, [...]
+    assert head_dim % 2 == 0  # "Dimension must be divisible by 2"
+    # Build the theta parameter
+    # According to the formula theta_i = 10000^(-2(i-1)/dim) for i = [1, 2, ... dim/2]
+    # Shape: (Head_Dim / 2)
+    theta_numerator = torch.arange(0, head_dim, 2).float()
+    # Shape: (Head_Dim / 2)
+    theta = 1.0 / (theta ** (theta_numerator / head_dim)).to(device)
+
+    # Construct the positions (the "m" parameter)
+    # Shape: (Seq_Len)
+    m = torch.arange(seq_length, device=device)
+
+    # Multiply each theta by each position using the outer product.
+    # Shape: (Seq_Len) outer_product* (Head_Dim / 2) -> (Seq_Len, Head_Dim / 2)
+    m_theta = torch.outer(m, theta).float()
+
+    # We can compute complex numbers in the polar form c = R * exp(m * theta), where R = 1 as follows:
+    # (Seq_Len, Head_Dim / 2) -> (Seq_Len, Head_Dim / 2)
+    m_theta_complex = torch.polar(torch.ones_like(m_theta), m_theta)
+    return m_theta_complex
 
 
 class Transformer(nn.Module):
@@ -49,7 +84,7 @@ class Transformer(nn.Module):
     def forward(self, tokens: torch.Tensor, start_pos: int):
         # (B, seq_length)
         batch_size, seq_len = tokens.shape
-        assert seq_len == 1 # only one token at a time can be processed.
+        assert seq_len == 1  # only one token at a time can be processed.
 
         # (B, seq_length) -> (B, seq_length, dim)
         h = self.tok_embeddings(tokens)
