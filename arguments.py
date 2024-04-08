70286b8356f772daee93f0daf4537973f4c2f4df
abdallah197 <abdullahbashir077@gmail.com>
2024-04-08 03:56:47 +0100

    added nvitop to requirements.txt

--- arguments.py
+++ arguments.py
@@ -15,6 +15,3 @@ def get_args():
     for config_args in [model_args, train_args, data_args, inference_args, deepspeed_args]:
         args.update(vars(config_args))
     return args
-
-
-get_args()
