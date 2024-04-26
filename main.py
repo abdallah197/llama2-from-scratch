6af995b754692c6f6017b7d8d1696a641b0a75fb
abdallah197 <abdullahbashir077@gmail.com>
2024-04-26 19:44:15 +0100

    added main function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import deepspeed
 import sentencepiece as spm
 
 from arguments import get_args
@@ -6,17 +7,25 @@ from data import TextFileDataset, create_dataloaders
 from model import Transformer
 from train import train
 
-args = get_args()
-tokenizer = spm.SentencePieceProcessor()
-tokenizer.load(args['tokenizer_model_path'])
-dataset = TextFileDataset(args['filepath'], tokenizer, args['max_seq_length'], pad_token_id=0)
-model_args = ModelArgs()
-model_args.vocab_size = tokenizer.vocab_size()
-train_dataloader, eval_dataloader = create_dataloaders(dataset, args['train_size'], args['batch_size'])
 
-model = Transformer(model_args)
-train(model=model,
-      train_config=TrainArgs(),
-      train_dataloader=train_dataloader,
-      eval_dataloader=eval_dataloader,
-      args=args)
+def run_main():
+      args = get_args()
+      tokenizer = spm.SentencePieceProcessor()
+      tokenizer.load(args['tokenizer_model_path'])
+      dataset = TextFileDataset(args['filepath'], tokenizer, args['max_seq_length'], pad_token_id=0)
+      model_args = ModelArgs()
+      model_args.vocab_size = tokenizer.vocab_size()
+      if args['deepspeed']:
+            deepspeed.init_distributed()
+      train_dataloader, eval_dataloader = create_dataloaders(dataset, args['train_size'], args['batch_size'])
+
+      model = Transformer(model_args)
+      train(model=model,
+            train_config=TrainArgs(),
+            train_dataloader=train_dataloader,
+            eval_dataloader=eval_dataloader,
+            args=args)
+
+
+if __name__ == '__main__':
+      run_main()
